2022199dfa97035ca88cd7380c84bd6f3bbf197f
The Other Tom <tkelley@bluedata.com>
2017-11-27 09:43:49 -0800

    sanitize the ID to an NCName by turning the first char of the UUID into a letter. To keep the randomness high, the letter will be in g to whatever is 6 after g. This is annoying, but it should be enough to unblock QA.

--- src/esaml_sp.erl
+++ src/esaml_sp.erl
@@ -26,9 +26,14 @@
 add_xml_id(Xml) ->
     Xml#xmlElement{attributes = Xml#xmlElement.attributes ++ [
         #xmlAttribute{name = 'ID',
-            value = uuid:to_string(uuid:uuid1()),
+            value = sanitize_to_ncname(uuid:to_string(uuid:uuid1())),
             namespace = #xmlNamespace{}}
         ]}.
+
+%% @private
+-spec sanitize_to_ncname(string()) -> string().
+sanitize_to_ncname([FLetter | UuidString]) ->
+    [FLetter + 55 | UuidString].
 
 %% @doc Return an AuthnRequest as an XML element
 -spec generate_authn_request(IdpURL :: string(), esaml:sp()) -> #xmlElement{}.
